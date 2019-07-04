2fae1e5d481a4fba74a72b0a68a5c08db8b69a05
BestKnow <52500949+BestKnow@users.noreply.github.com>
2019-07-04 23:29:04 +0800

    Add files via upload

--- han.ino
+++ han.ino
@@ -1,212 +1,76 @@
-#include <morse.h>
-Morse morse(13);
-int input=0;
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
   Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available()>0){
-    input=Serial.read();
-    switch(input){
-      case 'A':
-        morse.dot();
-        morse.dash();
-        morse.c_space();
-        break;
-      
-      case 'B':
-        morse.dash();
-        morse.dot();
-        morse.dot();
-        morse.dot();
-        morse.c_space();
-        break;
-      
-       case 'C':
-        morse.dash();
-        morse.dot();
-        morse.dash();
-        morse.dot();
-        morse.c_space();
-        break;
-      
-      
-      case 'D':
-        morse.dash();
-        morse.dot();
-        morse.dot();
-        morse.c_space();
-        break;
-      
-      case 'E':
-        morse.dot();
-        morse.c_space();
-        break;
-      
-       case 'F':
-        morse.dot();
-        morse.dot();
-        morse.dash();
-        morse.dot();
-        morse.c_space();
-        break;
-      
-       case 'G':
-        morse.dash();
-        morse.dash();
-        morse.dot();
-        
-        morse.c_space();
-        break;
-      
-       case 'H':
-        morse.dot();
-        morse.dot();
-        morse.dot();
-        morse.dot();
-        morse.c_space();
-        break;
-      
-      case 'I':
-        morse.dot();
-        morse.dot();
-      
-        morse.c_space();
-        break;
-      
-       case 'J':
-        morse.dot();
-        morse.dash();
-        morse.dash();
-        morse.dash();
-        morse.c_space();
-        break;
-      
-       case 'K':
-        morse.dash();
-        morse.dot();
-       
-        morse.dash();
-        morse.c_space();
-        break;
-      
-       case 'L':
-        morse.dot();
-        morse.dash();
-        morse.dot();
-        morse.dot();
-        morse.c_space();
-        break;
-      
-       case 'M':
-        morse.dash();
-        morse.dash();
-        
-        morse.c_space();
-        break;
-      
-       case 'N':
-        morse.dash();
-        morse.dot();
-        
-        morse.c_space();
-        break;
-      
-       case 'O':
-        morse.dash();
-        morse.dash();
-        morse.dash();
-       
-        morse.c_space();
-        break;
-      
-       case 'P':
-        morse.dot();
-        morse.dash();
-        morse.dash();
-        morse.dot();
-        morse.c_space();
-        break;
-      case 'Q':
-        morse.dash();
-        morse.dash();
-        morse.dot();
-        morse.dash();
-        morse.c_space();
-        break;
-       case 'R':
-        morse.dot();
-        morse.dash();
-        morse.dot();
-        morse.c_space();
-        break;
-      
-       case 'S':
-        morse.dot();
-        morse.dot();
-        morse.dot();
-        morse.c_space();
-        break;
-      
-       case 'T':
-        morse.dash();
-      
-       
-        morse.c_space();
-        break;
-      
-       case 'U':
-        morse.dot();
-        morse.dot();
-        morse.dash();
-        morse.c_space();
-        break;
-      
-      case 'V':
-        morse.dot();
-        morse.dot();
-        morse.dot();
-        morse.dash();
-        morse.c_space();
-        break;
-      
-       case 'W':
-        morse.dot();
-        morse.dash();
-        morse.dash();
-        morse.c_space();
-        break;
-      
-       case 'X':
-        morse.dash();
-        morse.dot();
-        morse.dot();
-        morse.dash();
-        morse.c_space();
-        break;
-      
-       case 'Y':
-        morse.dash();
-        morse.dot();
-        morse.dash();
-        morse.dash();
-        morse.c_space();
-        break;
-      
-       case 'Z':
-        morse.dash();
-        morse.dash();
-        morse.dot();
-        morse.dot();
-        morse.c_space();
-        break;
-      
-     
-     
+int income=0;
+void loop()
+{
+  if(Serial.available()>0)
+    {
+      income=Serial.read();
+        switch(income)
+        {
+          case 'f':
+              forward();
+              break;
+            case 'b':
+              backward();
+              break;
+            case 'l':
+              left();
+              break;
+            case 'r':
+              right();
+              break;
+            case 's':
+              stop();
+              break;
+            default:
+              break;
+        }
     }
-  }
+}
 
+void forward()
+{
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+}
+
+void backward()
+{
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+}
+
+void left()
+{
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+}
+
+void right()
+{
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+}
+
+void stop()
+{
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
 }
